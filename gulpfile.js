9f6446ae548a6d879374f66e3b2b27c010f6bcc6
picpoint <picpoint@yandex.ru>
2018-11-28 16:52:25 +0300

    path var

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,35 @@ var less = require('gulp-less');
 var smartgrid = require('smart-grid');
 var browserSync = require('browser-sync').create();
 
-gulp.task('test', function () {
-		console.log('RUN TEST TASK');
+const settings = {
+	root: './src',
+	dist: './dist',
+	srcless: './src/less/*.less',
+	srccss: './dist/css',
+	srchtml: './dist/*.html'
+};
+
+gulp.task('preproc', function () {
+	return gulp.src(settings.srcless)
+		.pipe(less())
+		.pipe(gulp.dest(settings.srccss))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 });
 
+gulp.task('html', function () {
+	gulp.src(settings.srchtml)
+		.pipe(gulp.dest(settings.dist))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
 
-
-
+gulp.task('watch', ['preproc', 'browserSync'], function () {
+	gulp.watch(settings.srcless, ['preproc']);
+	gulp.watch(settings.srchtml, ['html']);
+});
 
 
 
@@ -17,7 +39,7 @@ gulp.task('test', function () {
 gulp.task('browserSync', function () {
 		browserSync.init({
 			server: {
-					baseDir: './dist'
+					baseDir: settings.dist
 			}
 		});
 });
