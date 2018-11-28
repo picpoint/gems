823b2c54c1cbbe68631de4cde6ab5a934c3bd4f6
rmtar <picpoint@yandex.ru>
2018-11-28 23:40:43 +0300

    add mixins to less

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,21 @@ var browserSync = require('browser-sync').create();
 const settings = {
 	root: './src',
 	dist: './dist',
-	srcless: './src/less/*.less',
+	srcless: './src/less/style.less',
 	srccss: './dist/css',
-	srchtml: './dist/*.html'
+	srchtml: './dist/*.html',
+	srcjs: './src/js'
+};
+
+const smartGrigConf = {
+		outputStyle: 'less',
+		colums: 12,
+		offset: '10px',   // межклоночник
+		container: {
+				maxWidth: '1280px',
+				fields: '30px'   // отступ от края экрана
+		},
+		breakPoints: {}
 };
 
 gulp.task('preproc', function () {
@@ -28,12 +40,23 @@ gulp.task('html', function () {
 		}));
 });
 
-gulp.task('watch', ['preproc', 'browserSync'], function () {
+gulp.task('js', function () {
+		gulp.src(settings.srcjs + '/*.js')
+		.pipe(gulp.dest(settings.dist + '/js'))
+		.pipe(browserSync.reload({
+				stream: true
+		}));
+});
+
+gulp.task('watch', ['preproc', 'js', 'browserSync'], function () {
 	gulp.watch(settings.srcless, ['preproc']);
+	gulp.watch(settings.srcjs + '/*.js', ['js']);
 	gulp.watch(settings.srchtml, ['html']);
 });
 
-
+gulp.task('grid', function () {
+		smartgrid(settings.root + '/less', smartGrigConf);
+});
 
 
 gulp.task('browserSync', function () {
